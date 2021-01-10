b2a448e2df65ce694edad5ce57f74f10d8dc2a79
Luiz Chequini <luizchequini@gmail.com>
2021-01-10 19:50:45 -0300

    Aula de Modules e Decorators, aprendi um pouco em relação a criação de arquivos de módulos e também criação de arquivos de serviços

---
+++ src/app/funcionalidades/funcionalidades.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [
+    // Meus componentes
+  ],
+  imports: [
+    CommonModule
+    // Meus modulos
+  ],
+  providers: [
+    // Meus serviços
+  ]
+})
+export class FuncionalidadesModule { }
